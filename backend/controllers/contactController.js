26b412b73bf4a92c4a99efaf0ecc33ce01e99569
Otmane Aatik <aaotmane@gmail.com>
2023-04-11 00:04:35 -0400

    error handler middleware

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -7,25 +7,31 @@ const getContacts = (req, res) => {
 //@desc create new contact
 //@route POST /api/contacts
 //@access public
-const createContact = (req, res) => {
+const createContact = async (req, res) => {
+  console.log(req.body)
+  const {name, email, phone} = req.body;
+  if (!name || !email || !phone) {
+    res.status(400);
+    throw new Error("All fields are mandatory!")
+  }
   res.status(201).json({ message: "Create Contact" });
 };
 //@desc Get contact
 //@route GET /api/contacts/:id
 //@access public
-const getContact = (req, res) => {
+const getContact = async (req, res) => {
   res.status(201).json({ message: `Get contact with ID ${req.params.id}`});
 };
 //@desc Update contact
 //@route PUT /api/contacts/:id
 //@access public
-const updateContact = (req, res) => {
+const updateContact = async (req, res) => {
   res.status(200).json({ message: `Update Contact for ${req.params.id}` });
 };
 //@desc Delete contact
 //@route delete /api/contacts/:id
 //@access public
-const deleteContact = (req, res) => {
+const deleteContact = async (req, res) => {
   res.status(200).json({ message: `Delete Contact for ${req.params.id}` });
 };
 
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const errorHandler = require('./middleware/errorHandler');
 // const dotenv = require('dotenv').config();
 const app = express();
 
 const port = 3000;
 
-app.use("api/contacts", require("./routes/contactRoutes"));
+app.use(express.json())//Parser to retrieve JSON data from client side
+app.use("/api/contacts", require("./routes/contactRoutes"));
+app.use(errorHandler);
 
 
 
